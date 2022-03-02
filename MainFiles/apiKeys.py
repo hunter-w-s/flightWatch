b7ce7d8062c98d5c35d04c15695e03183358d3c9
hunter-w-s <53162440+hunter-w-s@users.noreply.github.com>
2022-03-01 22:51:59 -0500

    Update apiKeys.py

--- MainFiles/apiKeys.py
+++ MainFiles/apiKeys.py
@@ -1,5 +1,5 @@
-tweepyConsumeKey = "1Ae6znw6op7ZbfPc8f0wXXDMt"
-tweepyConsumeSecret = "zM7Aw2dNEeBHgpAUW0zqvOnSRTkjeBwKdrtP1PUXnOzRgBuOIT"
+tweepyConsumeKey = "TweepyConsumerKey"
+tweepyConsumeSecret = "TweepyConsumerSecret"
 
-tweepyAccessKey = "1497239674049511428-IV39RuVHFwFgBO4JlYLWU1ku4OKWe1"
-tweepyAccessSecret = "2C2oKZKjXNfDhH70UGSX5aeke3ik4Tv33AcZGWqKsuqfg"
+tweepyAccessKey = "TweepyAccessKey"
+tweepyAccessSecret = "TweepyAccessSecret"
